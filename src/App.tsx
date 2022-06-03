097b78fde9416c3a7427dfc0226fd6b3483f5506
Patrick Lønhaug <patrlo@hotmail.com>
2022-06-04 01:17:53 +0200

    Logo added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Buttons from "./components/home/Buttons";
 import Countdown from "./components/home/Countdown";
+import Logo from "./components/home/Logo";
 
 const App = () => {
   return (
     <div className="main flex flex-col justify-center items-center">
+      <Logo />
       <Countdown />
       <Buttons />
     </div>
---
+++ src/components/home/Logo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo = () => {
+  return <img src="/logo.webp"></img>;
+};
+
+export default Logo;
