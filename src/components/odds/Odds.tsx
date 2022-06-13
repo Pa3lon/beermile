5c284e2257794257b4f43bc17e0f1883ce99eb39
Patrick Lønhaug <patrlo@hotmail.com>
2022-06-13 22:36:16 +0200

    Made home-link

--- src/components/odds/Odds.tsx
+++ src/components/odds/Odds.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Games from "./Games";
 import SelectedGames from "./SelectedGames";
 
 const Odds = () => {
   const [games, setGames] = React.useState([]);
   return (
-    <div className="flex justify-between w-full pt-20 text-white">
+    <div className="flex text-red-100 justify-between w-full pt-20 text-white">
+      <Link className="absolute italic text-white top-5 left-5" to="/">
+        Hjem
+      </Link>
       <div></div>
       <Games games={games} />
       <SelectedGames />
