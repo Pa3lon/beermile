57af94b7db5ddae9ceff0c3b7038349fbf6905ad
Patrick Lønhaug <patrlo@hotmail.com>
2023-06-05 23:49:49 +0200

    New update

--- src/components/home/Buttons.tsx
+++ src/components/home/Buttons.tsx
@@ -1,15 +1,45 @@
 import { Button, Dialog } from "@mui/material";
-import React, { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const count2 = new Date("2023-07-01T00:00:00");
+const count3 = new Date("2023-06-05T00:05:00");
 
 const Buttons = () => {
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [trollOpen, setTrollOpen] = useState(false);
+  const [countdown, setCountdown] = useState("-:-:-:-");
+  const [competitorsOpen, setCompetitorsOpen] = useState(false);
+  const btn2ref = useRef<HTMLButtonElement>(null);
 
-  const join = () => {
-    alert("Hør med Sigurd");
+  const handleCompetitorsClick = () => {
+    if (countdown !== "Deltakere") return;
+    const c3 = getCountdown(count3);
+    c3 !== "Deltakere" ? setTrollOpen(true) : setCompetitorsOpen(true);
   };
 
-  const results = () => {
-    alert("Resultater blir ikke publisert pga dårlige resultater");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(getCountdown(count2));
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  const getCountdown = (time: Date) => {
+    const now = new Date();
+    const diff = time.getTime() - now.getTime();
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / 1000 / 60) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    // return 00:00:00:00 format when the countdown is over
+    if (diff < 0) {
+      // Add opacity to the button
+      btn2ref.current!.style.opacity = "1";
+      return "Deltakere";
+    }
+    // Retrun the countdown as a string
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
   };
 
   return (
@@ -23,9 +53,25 @@ const Buttons = () => {
         >
           Tidligere Resultater
         </Button>
+        <Button
+          ref={btn2ref}
+          onClick={() => handleCompetitorsClick()}
+          className="w-56"
+          style={{ opacity: 0.25, cursor: "not-allowed" }}
+          variant="contained"
+          color="primary"
+        >
+          {countdown}
+        </Button>
       </div>
       <Dialog onClose={() => setDialogOpen(false)} open={dialogOpen}>
         <img alt="guttan" src="/fav2.jpeg" />
+      </Dialog>
+      <Dialog onClose={() => setTrollOpen(false)} open={trollOpen}>
+        <img src="/fu_gif.gif" alt="fuck_u" />
+      </Dialog>
+      <Dialog onClose={() => setCompetitorsOpen(false)} open={competitorsOpen}>
+        <img src="/fu_gif.gif" alt="fuck_u" />
       </Dialog>
     </>
   );
